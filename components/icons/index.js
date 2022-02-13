1c044c171f7d215f6cdfb32bd25b3bffd722d65a
Urip Subekti <urip.subekti13@gmail.com>
2022-02-13 10:40:57 +0700

    fix: remove unused files

--- components/icons/index.js
+++ components/icons/index.js
@@ -5,5 +5,10 @@ export { default as Logout } from './logout';
 export { default as Document } from './document';
 export { default as LogoMark } from './logo-mark';
 export { default as LogoOnDark } from './logo-on-dark';
+export { default as Moon } from './moon';
+export { default as Sun } from './sun';
+export { default as Folder } from './folder';
+export { default as Home } from './home';
+export { default as Menu } from './menu';
+export { default as UserGroup } from './user-group';
 
-export * from './heroicons';
